fe4c83448fbe57bed39fe142dba5f309fdcb45cf
Samuel <myhonor16@users.noreply.github.com>
2014-10-07 22:07:48 -0400

    improved gulpfile & tweaked core:link color

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,63 @@
 var gulp = require('gulp'),
-	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minify = require('gulp-minify-css'),
 	rename = require("gulp-rename"),
 	concat = require('gulp-concat'),
-	sass = require('gulp-ruby-sass'),
-	minify = require('gulp-minify-css');
+	livereload = require('gulp-livereload'),
+	notify = require("gulp-notify"),
+	csslint = require('gulp-csslint');
 
 // Build
 gulp.task('build', function () {
 	gulp.src('./build/export.scss')
+		.pipe(plumber())
 		.pipe(sass({
 			style: 'expanded',
-			compass: true
+			compass: true,
+			sourcemap: false
+		}))
+		.pipe(autoprefixer({
+			cascade: false
 		}))
 		.pipe(gulp.dest('./dist/'))
 		.pipe(livereload({
 			auto: false
-		}));
+		}))
+		.pipe(notify("<%= file.relative %> - successful"));
 
 	gulp.src('./dist/export.css')
+		.pipe(plumber())
 		.pipe(minify())
 		.pipe(rename('basic.css'))
-		.pipe(gulp.dest('./dist/'))
+		.pipe(gulp.dest('./dist/'));
 
 	gulp.src(['./build/normalize.css', './dist/export.css'])
+		.pipe(plumber())
 		.pipe(concat('basic.norm.css'))
 		.pipe(minify())
-		.pipe(gulp.dest('./dist/'))
+		.pipe(gulp.dest('./dist/'));
+});
+
+// Test
+gulp.task('test', function() {
+	gulp.src('./dist/export.css')
+		.pipe(csslint({
+			"box-sizing": false,
+			"known-properties": false,
+			"regex-selectors": false,
+			"font-sizes": false,
+			"unqualified-attributes": false,
+			"compatible-vendor-prefixes": false
+		}))
+		.pipe(csslint.reporter());
 });
 
 gulp.task('watch', function() {
 	livereload.listen();
 
-	gulp.watch('build/**', ['build']);
+	gulp.watch('build/**', ['build','test']);
 	gulp.watch('demo/**/*.html').on('change', livereload.changed);
 });
 
